a5bcc3c2abd0ce00773eeb6addbae38f3a9a520a
paytondev <81662879+paytontech@users.noreply.github.com>
2025-05-04 13:17:24 -0400

    clock

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -81,13 +81,15 @@ void Sidebar::updateState(const UIState &s) {
 
   ItemStatus connectStatus;
   auto last_ping = deviceState.getLastAthenaPingTime();
-  if (last_ping == 0) {
-    connectStatus = ItemStatus{{tr("CONNECT"), tr("OFFLINE")}, warning_color};
-  } else {
-    connectStatus = nanos_since_boot() - last_ping < 80e9
-                        ? ItemStatus{{tr("CONNECT"), tr("ONLINE")}, good_color}
-                        : ItemStatus{{tr("CONNECT"), tr("ERROR")}, danger_color};
-  }
+  // if (last_ping == 0) {
+  //   connectStatus = ItemStatus{{tr("CONNECT"), tr("OFFLINE")}, warning_color};
+  // } else {
+  //   connectStatus = nanos_since_boot() - last_ping < 80e9
+  //                       ? ItemStatus{{tr("CONNECT"), tr("ONLINE")}, good_color}
+  //                       : ItemStatus{{tr("CONNECT"), tr("ERROR")}, danger_color};
+  // }
+  //show current time
+  connectStatus = ItemStatus({tr("TIME"), tr(QTime::currentTime().toString("hh:mm AP"))}, good_color);
   setProperty("connectStatus", QVariant::fromValue(connectStatus));
 
 #ifndef SUNNYPILOT
